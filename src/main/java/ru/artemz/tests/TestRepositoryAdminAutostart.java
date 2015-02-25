ebbfa67269fcccfe1a3dfb7ed84835f49b68eb46
Artem Zhirkov <az@develdynamic.com>
2015-02-25 20:46:00 +0300

    initial commit

---
+++ src/main/java/ru/artemz/tests/TestRepositoryAdminAutostart.java
@@ -0,0 +1,29 @@
+package ru.artemz.tests;
+import org.apache.felix.bundlerepository.RepositoryAdmin;
+import org.apache.felix.main.AutoProcessor;
+import org.osgi.framework.*;
+import org.osgi.framework.launch.Framework;
+import org.osgi.framework.launch.FrameworkFactory;
+import java.util.Map;
+import java.util.ServiceLoader;
+class TestRepositoryAdminAutostart {
+	public static void main(String[] args) throws Exception {
+		Framework framework = null;
+		BundleContext bundleContext;
+		RepositoryAdmin repositoryAdmin;
+		Map config = null;
+		FrameworkFactory frameworkFactory = ServiceLoader.load(FrameworkFactory.class).iterator().next();
+        	framework = frameworkFactory.newFramework(config);
+        	try {
+            	framework.init();
+	
+        	    AutoProcessor.process(config,framework.getBundleContext());
+	
+        	    framework.start();
+	        } catch (BundleException e){
+        	    e.printStackTrace();
+       		//TODO: log exceptions
+       		}
+        	bundleContext = framework.getBundleContext();	
+	}
+}
